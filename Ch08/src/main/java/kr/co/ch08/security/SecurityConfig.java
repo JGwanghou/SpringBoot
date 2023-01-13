20dd3206b1664f55a4eeda453dbe7dfe5b539ea4
jogwangho0619@gmail.com <jogwangho0619@gmail.com>
2023-01-14 01:32:50 +0900

    Security config 수정

--- Ch08/src/main/java/kr/co/ch08/security/SecurityConfig.java
+++ Ch08/src/main/java/kr/co/ch08/security/SecurityConfig.java
@@ -1,53 +1,55 @@
 package kr.co.ch08.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.MessageDigestPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
-	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		
-		// 인가(접근권한) 설정
-		http.authorizeHttpRequests().antMatchers("/").permitAll();
-		http.authorizeHttpRequests().antMatchers("/admin/**").hasRole("ADMIN");
-		http.authorizeHttpRequests().antMatchers("/member/**").hasAnyRole("ADMIN", "MEMBER");
-		http.authorizeHttpRequests().antMatchers("/user2/loginSuccess").hasAnyRole("3", "4", "5");
-		
-		// 사이트 위변조 요청 방지
-		http.csrf().disable();
-		
-		// 로그인 페이지 설정
-		http.formLogin() // form 기반의 로그인인 경우
-		.loginPage("/user2/login") // 인증이 필요한 URL에 접근하면 로그인폼으로 이동
-		.usernameParameter("uid") // 로그인 시 form에서 가져올 값
-		.passwordParameter("pass")// 로그인 시 form에서 가져올 값
-		.defaultSuccessUrl("/user2/loginSuccess") // 로그인 성공 시 이동할 페이지
-		.failureUrl("/user2/login?success=100"); // 로그인 실패 시 로그인페이지 이동
-		
-		
-		// 로그아웃 설정
-		http.logout() // 로그아웃 할 경우
-		.invalidateHttpSession(true) // 세션 삭제
-		.logoutRequestMatcher(new AntPathRequestMatcher("/user2/logout")) // 로그아웃을 처리할 URL
-		.logoutSuccessUrl("/user2/login?success=200"); // 로그아웃 성공 시 이동할 페이지
-	}
+@Configuration
+public class SecurityConfig {
 	
 	@Autowired
 	private SecurityUserService service;
 	
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	@Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		
-		// 로그인 인증처리 서비스 등록
-		auth.userDetailsService(service).passwordEncoder(new BCryptPasswordEncoder());
+		// 인가(접근권한) 설정
+		http.authorizeHttpRequests().antMatchers("/").permitAll(); // 모든 자원에 대해서 모든 사용자 접근 허용
+		http.authorizeHttpRequests().antMatchers("/admin/**").hasRole("ADMIN"); // admin 하위 모든 링크에 대해서 admin에게만 허용
+		http.authorizeHttpRequests().antMatchers("/member/**").hasAnyRole("ADMIN", "MEMBER"); // member 하위 모든 링크에 대해서 ADMIN, MEMBER 에게만 허용
+		http.authorizeHttpRequests().antMatchers("/user2/loginSuccess").hasAnyRole("3", "4", "5"); // /user2/loginSuccess는 ADMIN만 접근 허용
 		
+		// 사이트 위변조 요청 방지
+		http.csrf().disable();
+		
+		// 로그인 설정
+		http.formLogin()
+		.loginPage("/user2/login")
+		.defaultSuccessUrl("/user2/loginSuccess") // 로그인 성공 시 이동할 페이지
+		.failureUrl("/user2/login?success=100)")  // 로그인 실패 시 이동할 페이지
+		.usernameParameter("uid")
+		.passwordParameter("pass");
+		
+		// 로그아웃 설정
+		http.logout() // 로그아웃 할 경우
+		.invalidateHttpSession(true) // 세션 삭제
+		.logoutRequestMatcher(new AntPathRequestMatcher("/user2/logout")) // 로그아웃 처리할 URL
+		.logoutSuccessUrl("/user2/login?success=200"); // 로그아웃 성공 시 이동 할 페이지
+		
+		// 사용자 인증 처리 컴포넌트 서비스 등록
+		http.userDetailsService(service);
+		
+		return http.build();
 	}
+	
+	@Bean
+    public PasswordEncoder PasswordEncoder () {
+        //return new MessageDigestPasswordEncoder("SHA-256");
+		return new BCryptPasswordEncoder();
+    }
 }
