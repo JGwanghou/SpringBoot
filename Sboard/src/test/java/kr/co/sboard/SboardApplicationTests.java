bf45113e729b311159dc4c3e40f49ef6519193d6
JGwanghou <jogwangho0619@gmail.com>
2023-01-18 19:50:58 +0900

    Sboard firstPush

---
+++ Sboard/src/test/java/kr/co/sboard/SboardApplicationTests.java
@@ -0,0 +1,49 @@
+package kr.co.sboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import kr.co.sboard.dao.UserDAO;
+import kr.co.sboard.repository.UserRepo;
+import kr.co.sboard.vo.UserVO;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootTest
+class SboardApplicationTests {
+
+	
+	void contextLoads() {
+	}
+
+	@Autowired
+	private UserDAO dao;
+	
+	@Autowired
+	private UserRepo repo;
+	
+	public void insertTest() {
+		UserVO vo = UserVO.builder()
+					.uid("test02")
+					.pass1("1234")
+					.name("test01")
+					.nick("test01")
+					.email("test01")
+					.hp("test01")
+					.build();
+		int result = dao.insertUser(vo);
+		
+		log.info("result : " + result);
+		
+	}
+	
+	@Test
+	public void countUid() {
+		int result = repo.countByUid("letary");
+		
+		log.info("result : " + result);
+	}
+	
+	
+}
