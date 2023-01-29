1f57c435ef367612c5934af429d7a6fd6fbcadca
jogwangho0619@gmail.com <jogwangho0619@gmail.com>
2023-01-30 00:21:50 +0900

    20230130

--- Farmstory/src/main/java/kr/co/farmstory/controller/BoardController.java
+++ Farmstory/src/main/java/kr/co/farmstory/controller/BoardController.java
@@ -9,12 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -95,4 +93,5 @@ public class BoardController {
 
         return "redirect:/board/list?group="+group+"&cate="+cate;
     }
+
 }
